43af1a246c28dd002ccc7396ea021de82a7ef641
Kateryna Sydorenko <katerynnnna@gmail.com>
2024-07-05 20:40:32 +0300

    Set a timer for a quiz

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReplayIcon from '@mui/icons-material/Replay';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
 import { Box, Button, Container, Typography } from "@mui/material";
 
 import theme from "@/theme";
@@ -20,114 +21,143 @@ const Quiz = ({ title, questions }: QuizProps) => {
   const [showScore, setShowScore] = useState(false);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [clickedAnswerIndex, setClickedAnswerIndex] = useState<number | null>(null);
+  const [timeLeft, setTimeLeft] = useState('10:00');
 
-  const handleAnswerOptionClick = (index: number, isCorrect: boolean) => {
-    setClickedAnswerIndex(index);
+  useEffect(() => {
+    let totalSeconds = Number(timeLeft.split(':')[0]) * 60 + Number(timeLeft.split(':')[1]);
 
-    if (isCorrect) {
-      setScore(score + 1);
-    }
-  };
+    const timer = setInterval(() => {
+      if (totalSeconds <= 0) {
+        setShowScore(true);
+        clearInterval(timer);
+      }
 
-  const handleNextButton = () => {
-    setClickedAnswerIndex(null);
+      const minutes = Math.floor(totalSeconds / 60);
+      const seconds = totalSeconds % 60;
 
-    const nextQuestionIndex = currentQuestionIndex + 1;
+      setTimeLeft(`${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`);
+      totalSeconds -= 1;
+    }, 1000);
 
-    if (nextQuestionIndex < questions.length) {
-      setCurrentQuestionIndex(nextQuestionIndex);
-    } else {
-      setShowScore(true);
-    }
-  };
+      return () => clearInterval(timer);
+    }, [timeLeft])
 
-  const handleRestartButton = () => {
-    if (confirm('Are you sure? You lose your progress')) {
-      setScore(0);
-      setShowScore(false);
-      setCurrentQuestionIndex(0);
+    const handleAnswerOptionClick = (index: number, isCorrect: boolean) => {
+      setClickedAnswerIndex(index);
+
+      if (isCorrect) {
+        setScore(score + 1);
+      }
+    };
+
+    const handleNextButton = () => {
       setClickedAnswerIndex(null);
-    }
-  };
 
-  const getAnswerStyles = (index: number, answer: Answer) => {
-    if (index === clickedAnswerIndex && answer.isCorrect) {
-      return 'success.main';
-    } else if (index === clickedAnswerIndex && !answer.isCorrect) {
-      return 'error.main';
-    } else {
-      return '';
-    }
-  };
+      const nextQuestionIndex = currentQuestionIndex + 1;
 
-  return (
-    <Box sx={{
-      bgcolor: 'background.default',
-      width: '100%',
-      height: '100vh',
-      display: "flex",
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <Container sx={{
-        bgcolor: 'background.paper',
-        borderRadius: 3,
-        padding: 2,
-        width: '50%',
-        minHeight: 'min-content',
-        [theme.breakpoints.down('md')]: {
-          width: '90%',
-        },
-      }}>
-        <Box>
-          <Typography variant="h3" color="primary.main" sx={{ marginBottom: 3 }}>{title} Quiz</Typography>
+      if (nextQuestionIndex < questions.length) {
+        setCurrentQuestionIndex(nextQuestionIndex);
+      } else {
+        setShowScore(true);
+      }
+    };
+
+    const handleRestartButton = () => {
+      if (confirm('Are you sure? You lose your progress')) {
+        setScore(0);
+        setShowScore(false);
+        setCurrentQuestionIndex(0);
+        setClickedAnswerIndex(null);
+      }
+    };
+
+    const getAnswerStyles = (index: number, answer: Answer) => {
+      if (index === clickedAnswerIndex && answer.isCorrect) {
+        return 'success.main';
+      } else if (index === clickedAnswerIndex && !answer.isCorrect) {
+        return 'error.main';
+      } else {
+        return '';
+      }
+    };
+
+    return (
+      <Box
+        sx={{
+          bgcolor: 'background.default',
+          width: '100%',
+          height: '100vh',
+          display: "flex",
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Container
+          sx={{
+            bgcolor: 'background.paper',
+            borderRadius: 3,
+            padding: 2,
+            width: '50%',
+            minHeight: 'min-content',
+            [theme.breakpoints.down('md')]: {
+              width: '90%',
+            },
+          }}
+        >
           <Box>
-            {showScore ? <Score score={score} questionsLength={questions.length} /> : (
-              <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Typography variant="h6">Question {currentQuestionIndex + 1} of {questions.length}</Typography>
-                <Typography variant="subtitle1" sx={{ marginTop: 1 }}>{questions[currentQuestionIndex].question}</Typography>
-                <Box sx={{ display: "flex", flexDirection: 'column', marginBlock: 2, gap: 1 }}>
-                  {questions[currentQuestionIndex].answers.map((answer, index) => {
-                    const answerStyles = getAnswerStyles(index, answer);
-                    return (
-                      <Button
-                        key={index}
-                        disabled={clickedAnswerIndex !== null}
-                        onClick={() => handleAnswerOptionClick(index, answer.isCorrect)}
-                        sx={{
-                          bgcolor: answerStyles,
-                          border: 1,
-                          borderColor: 'background.default',
-                          textTransform: "initial",
-                          fontSize: '1rem',
-                        }}
-                        color='secondary'
-                      >
-                        {answer.text}
-                      </Button>
-                    )
-                  })}
+            <Typography variant="h3" color="primary.main" sx={{ marginBottom: 3 }}>{title} Quiz</Typography>
+            <Box>
+              {showScore ? <Score score={score} questionsLength={questions.length} /> : (
+                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <Box sx={{ display: "flex", justifyContent: 'space-between', alignItems: 'center' }}>
+                    <Typography variant="h6">Question {currentQuestionIndex + 1} of {questions.length}</Typography>
+                    <Typography variant="subtitle2" sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                      <AccessTimeOutlinedIcon fontSize="small" />{timeLeft}
+                    </Typography>
+                  </Box>
+                  <Typography variant="subtitle1" sx={{ marginTop: 1 }}>{questions[currentQuestionIndex].question}</Typography>
+                  <Box sx={{ display: "flex", flexDirection: 'column', marginBlock: 2, gap: 1 }}>
+                    {questions[currentQuestionIndex].answers.map((answer, index) => {
+                      const answerStyles = getAnswerStyles(index, answer);
+                      return (
+                        <Button
+                          key={index}
+                          disabled={clickedAnswerIndex !== null}
+                          onClick={() => handleAnswerOptionClick(index, answer.isCorrect)}
+                          sx={{
+                            bgcolor: answerStyles,
+                            border: 1,
+                            borderColor: 'background.default',
+                            textTransform: "initial",
+                            fontSize: '1rem',
+                          }}
+                          color='secondary'
+                        >
+                          {answer.text}
+                        </Button>
+                      )
+                    })}
+                  </Box>
+                  <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
+                    <Button
+                      variant="outlined"
+                      onClick={handleRestartButton}
+                    >Restart<ReplayIcon fontSize="small" />
+                    </Button>
+                    <Button
+                      onClick={handleNextButton}
+                      sx={{ paddingInline: 4, color: 'primary.contrastText' }}
+                      variant="contained">Next</Button>
+                  </Box>
+                  <ProgressBar currentQuestion={currentQuestionIndex} totalQuestions={questions.length} />
                 </Box>
-                <Box sx={{ display: 'flex', justifyContent: 'flex-end', gap: 2 }}>
-                  <Button
-                    variant="outlined"
-                    onClick={handleRestartButton}
-                  >Restart<ReplayIcon fontSize="small" />
-                  </Button>
-                  <Button
-                    onClick={handleNextButton}
-                    sx={{ paddingInline: 4, color: 'primary.contrastText' }}
-                    variant="contained">Next</Button>
-                </Box>
-                <ProgressBar currentQuestion={currentQuestionIndex} totalQuestions={questions.length} />
-              </Box>
-            )}
+              )}
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </Box>
-  )
-}
+        </Container>
+      </Box>
+    )
+  }
 
 export default Quiz;
