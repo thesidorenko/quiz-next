3b1b97ff47c11b68f6e4cc671944762f4e518110
Kateryna Sydorenko <katerynnnna@gmail.com>
2024-07-09 15:18:22 +0300

    Fix issue when assets don't load properly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: 'https://github.com/thesidorenko/quiz-next'
+};
 
 export default nextConfig;
