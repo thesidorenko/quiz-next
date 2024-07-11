2f47941df099af102c75e2ae9f68674ba7f71116
Kateryna Sydorenko <katerynnnna@gmail.com>
2024-07-11 15:18:18 +0300

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  basePath: isProd ? '/quiz-next' : '',
-  assetPrefix: isProd ? `/quiz-next` : '',
 };
 
 export default nextConfig;
